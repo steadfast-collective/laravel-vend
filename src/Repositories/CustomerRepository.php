af7f6e56671a22ec7a1fd4b4a9235042909ee5a6
Duncan McClean <duncan@steadfastcollective.com>
2020-09-02 09:31:17 +0100

    Add some typehinting to the repositories

--- src/Repositories/CustomerRepository.php
+++ src/Repositories/CustomerRepository.php
@@ -8,28 +8,28 @@ class CustomerRepository
 {
     private static $baseUrl = "customers/";
 
-    public static function index($filters)
+    public static function index(array $filters = [])
     {
         return resolve(ApiRequestor::class)->get(self::$baseUrl, $filters);
     }
 
-    public static function show($thirdPartyID, $data)
+    public static function show(string $thirdPartyID, array $data = [])
     {
         return resolve(ApiRequestor::class)->get(self::$baseUrl.$thirdPartyID, $data);
     }
 
-    public static function create($data)
+    public static function create(array $data = [])
     {
         return resolve(ApiRequestor::class)->post(self::$baseUrl, $data);
     }
 
-    public static function update($thirdPartyID, $data)
+    public static function update(string $thirdPartyID, array $data = [])
     {
         return resolve(ApiRequestor::class)->put(self::$baseUrl.$thirdPartyID, $data);
     }
 
-    public static function destroy($thirdPartyId, $data)
+    public static function destroy(string $thirdPartyId, array $data = [])
     {
-        return resolve(ApiRequestor::class)->delete(self::$baseUrl.$thirdPartyID, $data);
+        return resolve(ApiRequestor::class)->delete(self::$baseUrl.$thirdPartyId, $data);
     }
 }
--- src/Repositories/ProductRepository.php
+++ src/Repositories/ProductRepository.php
@@ -8,12 +8,12 @@ class ProductRepository
 {
     private static $baseUrl = "products/";
 
-    public static function index($filters)
+    public static function index(array $filters = [])
     {
         return resolve(ApiRequestor::class)->get(self::$baseUrl, $filters);
     }
 
-    public static function show($thirdPartyID, $data)
+    public static function show(string $thirdPartyID, array $data = [])
     {
         return resolve(ApiRequestor::class)->get(self::$baseUrl.$thirdPartyID, $data);
     }
